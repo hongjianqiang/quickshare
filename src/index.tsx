e503275fea1f73abdc9a4d4b04dd435874f876ac
Qiang 洪建强 <hongjq1@cn.midea.com>
2019-12-05 10:30:12 +0800

    添加了ESLint配置

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ interface Person {
     lastName: string;
 }
 
-function greeter(person: Person) {
+function greeter (person: Person) {
     return `Hello, ${person.firstName} ${person.lastName}`;
 }
 
