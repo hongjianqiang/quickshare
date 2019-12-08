a8b70b48b9fee4577782a854141c112ff93049cc
Qiang 洪建强 <hongjq1@cn.midea.com>
2019-12-08 08:42:34 +0800

    删除tsx类型文件

--- src/index.d.ts
+++
@@ -1,9 +0,0 @@
-interface Person {
-    firstName: string;
-    lastName: string;
-}
-declare function greeter(person: Person): string;
-declare const user: {
-    firstName: string;
-    lastName: string;
-};
