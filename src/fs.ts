143d34d7b3692878d2e5b7430a39022ad0b4a4f3
Qiang 洪建强 <hongjq1@cn.midea.com>
2019-12-08 21:32:42 +0800

    修改了exists()函数的实现，添加了readdir()函数

--- src/fs.ts
+++ src/fs.ts
@@ -2,12 +2,26 @@ import fs from 'fs';
 
 export function exists (path: fs.PathLike): Promise<boolean> {
     return new Promise((resolve) => {
-        resolve(fs.existsSync(path));
+        fs.access(path, fs.constants.R_OK, (err) => {
+            err ? resolve(false) : resolve(true);
+        });
     });
 }
 
 export function stat (path: fs.PathLike): Promise<fs.Stats> {
-    return new Promise((resolve) => {
-        resolve(fs.statSync(path));
+    return new Promise((resolve, reject) => {
+        fs.stat(path, (err, stats) => {
+            if (err) reject(err);
+            resolve(stats);
+        });
+    });
+}
+
+export function readdir (path: fs.PathLike): Promise<string[]> {
+    return new Promise((resolve, reject) => {
+        fs.readdir(path, (err, files) => {
+            if (err) reject(err);
+            resolve(files);
+        });
     });
 }
