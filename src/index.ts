46129de11e90f2c066965e1f2ce0317148e9ae8d
Qiang 洪建强 <hongjq1@cn.midea.com>
2019-12-09 15:07:17 +0800

    修复了一个获取文件执行路径的Bug

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,38 @@
 import fs from 'fs';
 import path from 'path';
+import process from 'process';
 import { URL } from 'url';
 import http from 'http';
 import template from './template.html';
 import { getMime } from './mime-types';
-import { exists, stat } from './fs';
+import { exists, stat, readdir } from './fs';
 import compiler from './compiler';
 
 const PORT = 2020;
 const HOST = '0.0.0.0';
 const CHARSET = 'UTF-8';
 
-function handleDirs (req: http.IncomingMessage, res: http.ServerResponse, absPathname: string) {
-    console.log(compiler);
+async function handleDirs (req: http.IncomingMessage, res: http.ServerResponse, absPathname: string) {
+    const files = await readdir(absPathname);
+
+    let list:any[] = [];
+
+    list = files.map(async file => {
+        const filePath = path.join(absPathname, file);
+        const fileStat = await stat(filePath);
+        const isDir = fileStat.isDirectory();
+
+        return isDir ? file + path.sep : file;
+    });
+
+    list = await Promise.all(list);
+
+    const html = compiler(template, {
+        list
+    });
+
+    res.writeHead(200, { 'Content-Type': `${getMime('html')};charset=${CHARSET}` });
+    res.end(html);
 }
 
 function handleFiles (req: http.IncomingMessage, res: http.ServerResponse, absPathname: string) {
@@ -31,10 +51,10 @@ function handleFiles (req: http.IncomingMessage, res: http.ServerResponse, absPa
 }
 
 async function handleStatic (req: http.IncomingMessage, res: http.ServerResponse) {
-    const href = `http://${req.headers.host}${req.url}:${PORT}`;
+    const href = `http://${req.headers.host}${req.url}`;
     const url = new URL(href);
     const pathname = decodeURIComponent(url.pathname.replace(/\.\.\//g, ''));
-    const absPathname = path.join(__dirname, pathname);
+    const absPathname = path.join(process.cwd(), pathname);
 
     if (await exists(absPathname)) {
         if ((await stat(absPathname)).isDirectory()) {
@@ -49,7 +69,7 @@ async function handleStatic (req: http.IncomingMessage, res: http.ServerResponse
 }
 
 function requestListener (req: http.IncomingMessage, res: http.ServerResponse): void {
-    const href = decodeURIComponent(`http://${req.headers.host}${req.url}:${PORT}`);
+    const href = decodeURIComponent(`http://${req.headers.host}${req.url}`);
 
     console.log(req.method, href);
 
