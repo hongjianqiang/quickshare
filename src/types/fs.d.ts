45ea9e637e8f4497bb9ca4e38082dc48b2ae7c12
Qiang 洪建强 <hongjq1@cn.midea.com>
2019-12-09 15:06:09 +0800

    类型修改

--- src/types/fs.d.ts
+++ src/types/fs.d.ts
@@ -2,3 +2,4 @@
 import fs from 'fs';
 export declare function exists(path: fs.PathLike): Promise<boolean>;
 export declare function stat(path: fs.PathLike): Promise<fs.Stats>;
+export declare function readdir(path: fs.PathLike): Promise<string[]>;
