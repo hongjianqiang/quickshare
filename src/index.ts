8773ceb312ef497e41514c03639e8ad52447d7dc
hongjianqiang <569250030@qq.com>
2019-12-15 23:18:14 +0800

    优化了一下样式

--- src/index.ts
+++ src/index.ts
@@ -8,13 +8,36 @@ import { getMime } from './mime-types';
 import { exists, stat, readdir } from './fs';
 import compiler from './compiler';
 
-const PORT = 2020;
-const HOST = '0.0.0.0';
-const CHARSET = 'UTF-8';
+const ARGV = process.argv;
+
+let PORT:number;
+let HOST:string;
+let CHARSET:string;
+let ROOT:string;
+
+(function () {
+    let indexOf:number;
+
+    // 自定义端口
+    indexOf = ARGV.indexOf('-p');
+    PORT = indexOf < 0 ? 2020 : +ARGV[indexOf + 1];
+
+    // 自定义host
+    indexOf = ARGV.indexOf('-h');
+    HOST = indexOf < 0 ? '0.0.0.0' : ARGV[indexOf + 1];
+
+    // 自定义字符集
+    indexOf = ARGV.indexOf('-c');
+    CHARSET = indexOf < 0 ? 'UTF-8' : ARGV[indexOf + 1];
+
+    // 自定义根目录
+    indexOf = ARGV.indexOf('-r');
+    ROOT = indexOf < 0 ? process.cwd() : ARGV[indexOf + 1];
+})();
 
 async function handleDirs (req: http.IncomingMessage, res: http.ServerResponse, absPathname: string) {
     const files = await readdir(absPathname);
-    const pathname = absPathname.split(process.cwd()).join('');
+    const pathname = absPathname.split(ROOT).join('');
 
     let list:any[] = [];
 
@@ -38,10 +61,14 @@ async function handleDirs (req: http.IncomingMessage, res: http.ServerResponse, 
         return '/' + folders.slice(0, i + 1).join('/') + '/';
     }).map(path => ({
         href: path,
-        name: path.split('/').slice(-2).join('/')
+        name: path.split('/').slice(-2).join('')
     }));
 
+    const len = paths.length;
+    const title = (paths[len - 1] && paths[len - 1].name.slice(0, -1)) || '/';
+
     const html = compiler(template, {
+        title,
         paths,
         list
     });
@@ -69,7 +96,7 @@ async function handleStatic (req: http.IncomingMessage, res: http.ServerResponse
     const href = `http://${req.headers.host}${req.url}`;
     const url = new URL(href);
     const pathname = decodeURIComponent(url.pathname.replace(/\.\.\//g, ''));
-    const absPathname = path.join(process.cwd(), pathname);
+    const absPathname = path.join(ROOT, pathname);
 
     if (await exists(absPathname)) {
         if ((await stat(absPathname)).isDirectory()) {
