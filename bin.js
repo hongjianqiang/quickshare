27d6dbc0ab5e2dba1501732403e85232b3e2e3b5
Qiang 洪建强 <hongjq1@cn.midea.com>
2020-04-06 20:42:33 +0800

    修改了一下 -d 选项描述

--- bin.js
+++ bin.js
@@ -7,7 +7,7 @@ commander
     .name('quickshare')
     .version(package.version)
     .option('-p, --port [PORT]', 'Specify alternate port [default: 2020]')
-    .option('-d, --dir [DIR]', 'Specify alternative directory [default:current directory]')
+    .option('-d, --dir [DIR]', 'Specify alternative directory [default: current directory]')
     .parse(process.argv);
 
 require('./lib/httpshare.min.js');
