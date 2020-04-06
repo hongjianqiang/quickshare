5bf6881d44d1671d95f73a6898c9a651b71ed2ec
hongjianqiang <569250030@qq.com>
2020-04-06 20:24:24 +0800

    修改了用法

--- bin.js
+++ bin.js
@@ -3,15 +3,11 @@
 const package = require('./package.json');
 const commander = require('commander');
 
-const cmd = commander
+commander
+    .name('quickshare')
     .version(package.version)
     .option('-p, --port [PORT]', 'Specify alternate port [default: 2020]')
     .option('-d, --dir [DIR]', 'Specify alternative directory [default:current directory]')
     .parse(process.argv);
 
-if (process.argv.slice(2).length === 0) {
-    cmd.outputHelp();
-    process.exit();
-} else {
-    require('./lib/httpshare.min.js');
-}
+require('./lib/httpshare.min.js');
