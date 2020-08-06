5d3abc170199f9c2b66c4d8b6f3559219e710afd
Qiang 洪建强 <hongjq1@cn.midea.com>
2020-08-06 18:13:43 +0800

    ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,17 +4,21 @@ module.exports = {
     node: true,
     commonjs: true
   },
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: 'module',                   // use ESM syntax
+    sourceType: 'module',                           // use ESM syntax
     ecmaFeatures: {
-      impliedStrict: true,                  // enable global strict mode
-      jsx: true                             // enable JSX
+      impliedStrict: true,                          // enable global strict mode
+      jsx: true                                     // enable JSX
     }
   },
   globals: {
   },
+  plugins: ['@typescript-eslint'],
   rules: {
   },
-  extends: 'standard'                       // https://github.com/standard/standard/blob/master/docs/RULES-zhcn.md
+  extends: [
+    'standard',                                     // https://github.com/standard/standard/blob/master/docs/RULES-zhcn.md
+  ]
 };
--- src/app.ts
+++ src/app.ts
@@ -1,45 +1,45 @@
-import http from 'http';
-import Vue from 'vue';
-import config from '../package.json';
+import http from 'http'
+import Vue from 'vue'
+import config from '../package.json'
 
 const renderer = require('vue-server-renderer').createRenderer({
-  template: 
-  '<html lang="zh">'+
-  '  <head>'+
-  '    <meta charset="UTF-8">'+
-  ''+
-  '    <!-- 使用双花括号(double-mustache)进行 HTML 转义插值(HTML-escaped interpolation) -->'+
-  '    <title>{{ title }}</title>'+
-  ''+
-  '    <!-- 使用三花括号(triple-mustache)进行 HTML 不转义插值(non-HTML-escaped interpolation) -->'+
-  '    {{{ metas }}}'+
-  '  </head>'+
-  '  <body>'+
-  '    <!--vue-ssr-outlet-->'+
-  '  </body>'+
+  template:
+  '<html lang="zh">' +
+  '  <head>' +
+  '    <meta charset="UTF-8">' +
+  '' +
+  '    <!-- 使用双花括号(double-mustache)进行 HTML 转义插值(HTML-escaped interpolation) -->' +
+  '    <title>{{ title }}</title>' +
+  '' +
+  '    <!-- 使用三花括号(triple-mustache)进行 HTML 不转义插值(non-HTML-escaped interpolation) -->' +
+  '    {{{ metas }}}' +
+  '  </head>' +
+  '  <body>' +
+  '    <!--vue-ssr-outlet-->' +
+  '  </body>' +
   '</html>'
-});
+})
 
 const context = {
   title: `${config.name[0].toUpperCase()}${config.name.slice(1)}`,
   metas:
-  `  <meta name="keyword" content="vue,ssr">`+
+  '  <meta name="keyword" content="vue,ssr">' +
   `  <meta name="description" content="${config.description}">`
-};
+}
 
 http.createServer(function (req, res) {
   const app = new Vue({
     data: { url: req.url },
-    template: `<div>访问的 URL 是： {{ url }}</div>`
+    template: '<div>访问的 URL 是： {{ url }}</div>'
   })
 
   renderer.renderToString(app, context, (err:any, html:any) => {
     console.log(html)
 
     if (err) {
-      res.end(`Internal Server Error`)
-      return;
+      res.end('Internal Server Error')
+      return
     }
     res.end(html)
   })
-}).listen(2020, () => console.log('服务在运行了'));
+}).listen(2020, () => console.log('服务在运行了'))
