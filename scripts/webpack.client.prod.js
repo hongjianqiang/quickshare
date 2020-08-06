20881dd5c443b57fd7d268661f94da6df8cad06a
Qiang 洪建强 <hongjq1@cn.midea.com>
2020-08-06 10:05:30 +0800

    清理工程

--- scripts/webpack.client.prod.js
+++
@@ -1,6 +0,0 @@
-const { merge } = require('webpack-merge');
-const base = require('./webpack.client.base.js');
-
-module.exports = merge(base, {
-  mode: 'production'
-});
--- src/entry-client/index.ts
+++
@@ -1,3 +0,0 @@
-import Vue from 'vue'
-// import App from './App'
-console.log(Vue)
