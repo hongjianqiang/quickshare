9d3a9d052c98d326cf1d706bddb36470598edcf0
Qiang 洪建强 <hongjq1@cn.midea.com>
2021-01-15 18:05:07 +0800

    启用 JSX 语法

--- .babelrc.js
+++ .babelrc.js
@@ -2,7 +2,10 @@ module.exports = function (api) {
   api.cache.forever()
 
   return {
-    presets: ['@babel/preset-env'],
+    presets: [
+      '@babel/preset-env',
+      '@vue/babel-preset-jsx'
+    ],
     plugins: []
   }
 }
--- client/App.vue
+++ client/App.vue
@@ -1,3 +1,7 @@
-<template>
-  <h1>App Page</h1>
-</template>
+<script>
+  export default {
+    render (h) {
+      return <h2>JSX启动!</h2>
+    }
+  }
+</script>
