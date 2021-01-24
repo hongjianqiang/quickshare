b4892836480ef0d3b4f8f76c0832d9d5c59ea8dd
洪建强 <pn15014611087@gmail.com>
2021-01-24 22:42:15 +0800

    完善 Alert 组件

--- client/components/alert/index.vue
+++ client/components/alert/index.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="rel">
-    警告
+    <div class="abs alert" :style="`z-index: ${$zIndex}`">
+      警告
+    </div>
   </div>
 </template>
+
+<style lang="less" scoped>
+  .alert {
+    color: red;
+    font-size: 1rem;
+    display: inline-block;
+    top: 50%;
+    left: 50%;
+    transform: translateX(-50%) translateY(-50%);
+  }
+</style>
