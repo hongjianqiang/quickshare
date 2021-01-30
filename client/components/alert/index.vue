c16990754f116640af1aef3517de99c167a21de2
洪建强 <pn15014611087@gmail.com>
2021-01-30 10:40:47 +0800

    完善工程项目

--- client/components/alert/index.vue
+++ client/components/alert/index.vue
@@ -1,18 +1,54 @@
 <template>
   <div class="rel">
-    <div class="abs alert" :style="`z-index: ${$zIndex}`">
-      警告
+    <div class="fixed modal" :style="`z-index: ${$zIndex}`">
+      <div class="fixed box">
+        <div class="header">
+          <div class="title">标题名称</div>
+          <div class="close"></div>
+        </div>
+        <div class="content">
+          这是一段内容
+        </div>
+        <div class="bottom">
+          <v-button>确定</v-button>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <style lang="less" scoped>
-  .alert {
-    color: red;
-    font-size: 1rem;
-    display: inline-block;
+  @import '@/styles/var.less';
+
+  .modal {
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    background-color: fade(@black, @disabled-opacity * 100);
+  }
+  .box {
+    background: @white;
+    border-radius: @border-radius-md;
     top: 50%;
     left: 50%;
     transform: translateX(-50%) translateY(-50%);
+    min-width: 3rem;
+    max-width: 4rem;
+
+    .header {
+      padding: @padding-sm @padding-md;
+      font-size: @font-size-lg;
+      color: @text-color;
+    }
+    .content {
+      padding: @padding-sm @padding-md;
+      font-size: @font-size-md;
+      color: @text-color;
+    }
+    .bottom {
+      padding: @padding-sm @padding-md;
+      text-align: right;
+    }
   }
 </style>
